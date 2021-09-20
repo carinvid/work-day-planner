8895063c2785d3e5902904115210619df3ab0467
Carolina Reedy <carinvid@gmail.com>
2021-09-20 11:22:56 -0600

    fix typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,22 @@
 $("#currentDay").text(moment().format("llll"));
 
 // Initialize empty array for saved events
-var events = JSON.parse(localStorage.getItem("events")) || [];
-var checkStorage = localStorage.getItem("events");
+var activities = JSON.parse(localStorage.getItem("activities")) || [];
+var checkStorage = localStorage.getItem("activities");
 var startTime = 9;
 var currentTime = parseInt(moment().format("H"));
 
-/ Generate time blocks for 9 am to 5 pm
+// Generate time blocks for 9 am to 5 pm
 for (let index = 0; index < 9; index++) {
+  // Create row
+  var divRow = $('<div class="row">');
+  divRow.attr("data-id", index);
+  $(".container").append(divRow);
 
-    // Create row
-    var divRow = $('<div class="row">');
-    divRow.attr('data-id', index);
-    $('.container').append(divRow);
-
-    if (checkStorage === null) {
-
-        // Create objects in events array with index as id for local storage
-        events.push({ eventId: index });
-        console.log(events);
-        localStorage.setItem('events', JSON.stringify(events));
-        
-    };
-
+  if (checkStorage === null) {
+    // Create objects in events array with index as id for local storage
+    activities.push({ activitieId: index });
+    console.log(activities);
+    localStorage.setItem("events", JSON.stringify(activities));
+  }
+}
