62647f2cec020f7d9d66ea9fde3d0bac8395deaf
Carolina Reedy <carinvid@gmail.com>
2021-09-21 00:16:00 -0600

    correct typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@
 $("#currentDay").text(moment().format("llll"));
 
 // Initialize empty array for saved events
-var activities = JSON.parse(localStorage.getItem("activities")) || [];
-var checkStorage = localStorage.getItem("activities");
+var events = JSON.parse(localStorage.getItem("events")) || [];
+var checkStorage = localStorage.getItem("events");
 var startTime = 9;
 var currentTime = parseInt(moment().format("H"));
 
@@ -16,9 +16,9 @@ for (let index = 0; index < 9; index++) {
 
   if (checkStorage === null) {
     // Create objects in events array with index as id for local storage
-    activities.push({ activitieId: index });
-    console.log(activities);
-    localStorage.setItem("events", JSON.stringify(activities));
+    events.push({ eventId: index });
+    console.log(events);
+    localStorage.setItem("events", JSON.stringify(events));
   }
   // Populate first column with time
   var tableTime = $('<div class="hour col-1">');
@@ -31,4 +31,21 @@ for (let index = 0; index < 9; index++) {
   eventDesc.attr("data-time", startTime);
 
   setDescClass();
+  var eventTextArea = $('<textarea cols="65" rows="3"></textarea>');
+  eventDesc.html(eventTextArea);
+  eventTextArea.attr("data-id", index);
+  divRow.append(eventDesc);
+
+  // Get event description out of local storage to display in text event
+  var eventText = events[index].description;
+  eventTextArea.val(eventText);
+
+  // Create save buttons
+  var saveDiv = $('<div class="saveBtn col-1">');
+  divRow.append(saveDiv);
+  var saveBtn = $('<i class="far fa-save fa-md"></i>');
+  saveDiv.html(saveBtn);
+  saveBtn.attr("data-id", index);
+
+  startTime++;
 }
